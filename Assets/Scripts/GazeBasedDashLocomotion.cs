2f035656f2b06be5493ba00ec80a1787da57d749
caasted <caasted@gmail.com>
2016-12-16 13:35:09 -0800

    feat: Clean up references to teleportation

--- Assets/Scripts/GazeBasedDashLocomotion.cs
+++ Assets/Scripts/GazeBasedDashLocomotion.cs
@@ -6,7 +6,6 @@ public class GazeBasedDashLocomotion : MonoBehaviour {
 
     // This script is largely a modification of SteamVR_Teleporter.cs from the SteamVR Plugin by Valve Corporation
 
-    public bool teleportOnClick = false;
     public float movementSpeed = 10.0f;
 
     Transform reference
@@ -28,52 +27,48 @@ public class GazeBasedDashLocomotion : MonoBehaviour {
             trackedController = gameObject.AddComponent<SteamVR_TrackedController>();
         }
 
-        // trackedController.TriggerClicked += new ClickedEventHandler(DoClick);
-        trackedController.PadClicked += new ClickedEventHandler(DoClick);
+        trackedController.PadClicked += new ClickedEventHandler(SetDestination);
 
         // Start the player at the level of the terrain
-        var t = reference;
-        if (t != null)
-            t.position = new Vector3(t.position.x, Terrain.activeTerrain.SampleHeight(t.position), t.position.z);
+        var player = reference;
+        if (player != null)
+            player.position = new Vector3(player.position.x, Terrain.activeTerrain.SampleHeight(player.position), player.position.z);
     }
 
-    void DoClick(object sender, ClickedEventArgs e)
+    void SetDestination(object sender, ClickedEventArgs e)
     {
-        if (teleportOnClick)
+        var player = reference;
+        if (player == null)
+            return;
+
+        float refY = player.position.y;
+
+        Ray ray = new Ray(this.transform.position, transform.forward);
+
+        bool hasGroundTarget = false;
+        float dist = 0f;
+        RaycastHit hitInfo;
+        TerrainCollider tc = Terrain.activeTerrain.GetComponent<TerrainCollider>();
+        hasGroundTarget = tc.Raycast(ray, out hitInfo, 1000f);
+        dist = hitInfo.distance;
+
+        if (hasGroundTarget)
         {
-            var t = reference;
-            if (t == null)
-                return;
-
-            float refY = t.position.y;
-
-            Ray ray = new Ray(this.transform.position, transform.forward);
-
-            bool hasGroundTarget = false;
-            float dist = 0f;
-            RaycastHit hitInfo;
-            TerrainCollider tc = Terrain.activeTerrain.GetComponent<TerrainCollider>();
-            hasGroundTarget = tc.Raycast(ray, out hitInfo, 1000f);
-            dist = hitInfo.distance;
-
-            if (hasGroundTarget)
-            {
-                Vector3 headPosOnGround = new Vector3(SteamVR_Render.Top().head.localPosition.x, 0.0f, SteamVR_Render.Top().head.localPosition.z);
-                destination = ray.origin + ray.direction * dist - new Vector3(t.GetChild(0).localPosition.x, 0f, t.GetChild(0).localPosition.z) - headPosOnGround;
-            }
+            Vector3 headPosOnGround = new Vector3(SteamVR_Render.Top().head.localPosition.x, 0.0f, SteamVR_Render.Top().head.localPosition.z);
+            destination = ray.origin + ray.direction * dist - new Vector3(player.GetChild(0).localPosition.x, 0f, player.GetChild(0).localPosition.z) - headPosOnGround;
         }
     }
 
     // Update is called once per frame
     void Update () {
-        var t = reference;
-        if (t == null)
+        var player = reference;
+        if (player == null)
             return;
 
         if (destination == null)
             return;
 
-        t.position = t.position + (destination - t.position) * Time.fixedDeltaTime * movementSpeed;
+        player.position = player.position + (destination - player.position) * Time.fixedDeltaTime * movementSpeed;
 
 	}
 }
