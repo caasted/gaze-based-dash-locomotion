731322803bf3999ccc15686d2cf6a3f6c458c773
caasted <caasted@gmail.com>
2016-12-18 09:56:31 -0800

    feat: Improve targeting and movement mechanics

--- Assets/Scripts/GazeBasedDashLocomotion.cs
+++ Assets/Scripts/GazeBasedDashLocomotion.cs
@@ -10,7 +10,7 @@ public class GazeBasedDashLocomotion : MonoBehaviour {
 
     public float maxRange = 10.0f;
     public float movementSpeed = 10.0f;
-    float speedFactor = 4.0f; // To make movementSpeed approximately meters per second
+    float speedFactor = 3.2f; // To make movementSpeed approximately meters per second
 
     public GameObject trackedHMD = null;
     Vector3 destination;
@@ -42,23 +42,17 @@ public class GazeBasedDashLocomotion : MonoBehaviour {
 
     void SetDestination(object sender, ClickedEventArgs e)
     {
-        var player = reference;
-        if (player == null)
-            return;
-
         Ray ray = new Ray(trackedHMD.transform.position, trackedHMD.transform.forward);
 
         bool hasGroundTarget = false;
-        float dist = 0f;
         RaycastHit hitInfo;
         TerrainCollider tc = Terrain.activeTerrain.GetComponent<TerrainCollider>();
-        hasGroundTarget = tc.Raycast(ray, out hitInfo, 1000f);
-        dist = hitInfo.distance;
+        hasGroundTarget = tc.Raycast(ray, out hitInfo, maxRange);
 
         if (hasGroundTarget)
         {
             Vector3 headPosOnGround = new Vector3(SteamVR_Render.Top().head.localPosition.x, 0.0f, SteamVR_Render.Top().head.localPosition.z);
-            destination = ray.origin + ray.direction * dist - new Vector3(player.GetChild(0).localPosition.x, 0f, player.GetChild(0).localPosition.z) - headPosOnGround;
+            destination = hitInfo.point - headPosOnGround;
         }
     }
 
@@ -69,8 +63,9 @@ public class GazeBasedDashLocomotion : MonoBehaviour {
             return;
 
         Vector3 direction = destination - player.position;
-        if (direction.magnitude > 0.1) // Prevent errors
+        if (direction.magnitude > 0f) // Avoid NaN errors
         {
+            // If the step size is greater than the distance, teleport, otherwise step towards destination
             if (direction.magnitude > 2 * Time.fixedDeltaTime * movementSpeed * speedFactor)
                 player.position = player.position + (direction / direction.magnitude) * Time.fixedDeltaTime * movementSpeed * speedFactor;
             else
--- Assets/Scripts/GazeGroundTargeting.cs
+++ Assets/Scripts/GazeGroundTargeting.cs
@@ -7,15 +7,6 @@ public class GazeGroundTargeting : MonoBehaviour {
     public float maxRange = 10.0f;
     public GameObject target = null;
 
-    Transform reference
-    {
-        get
-        {
-            var top = SteamVR_Render.Top();
-            return (top != null) ? top.origin : null;
-        }
-    }
-
     // Use this for initialization
     void Start () {
         if (target == null)
@@ -29,23 +20,16 @@ public class GazeGroundTargeting : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        var player = reference;
-        if (player == null)
-            return;
-
         Ray ray = new Ray(this.transform.position, this.transform.forward);
 
         bool hasGroundTarget = false;
-        float dist = 0f;
         RaycastHit hitInfo;
         TerrainCollider tc = Terrain.activeTerrain.GetComponent<TerrainCollider>();
         hasGroundTarget = tc.Raycast(ray, out hitInfo, maxRange);
-        dist = hitInfo.distance;
 
         if (hasGroundTarget)
         {
-            Vector3 headPosOnGround = new Vector3(SteamVR_Render.Top().head.localPosition.x, 0.0f, SteamVR_Render.Top().head.localPosition.z);
-            target.transform.position = ray.origin + ray.direction * dist - new Vector3(player.GetChild(0).localPosition.x, 0f, player.GetChild(0).localPosition.z) - headPosOnGround;
+            target.transform.position = hitInfo.point;
             target.SetActive(true);
         } else
         {
